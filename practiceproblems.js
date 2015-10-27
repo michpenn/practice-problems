4dba650cedb432540f0fc074845181aeb6c80573
michpenn <michpenn@icloud.com>
2015-10-27 16:30:40 -0700

    practice problem 3

--- practiceproblems.js
+++ practiceproblems.js
@@ -7,7 +7,6 @@
  */
 var arrayOfStrings = ['bug', 'insect', 'mammal', 'reptile', 'mammoth', 'turtles'];
 
-
 function biggerWords(theString, theArray) {
     var longWord = '';
     var longArray = [];
@@ -26,15 +25,52 @@ biggerWords('hello', arrayOfStrings);
  * Practice Problem 2
  * @param anArrayToReverse
  */
+var myArray = ['hello', 45, 'Bob', 'what', '23'];
+
 function arrayReverse(anArrayToReverse) {
-    var reversedArray=[];
+    var reversedArray = [];
     var wordHolder;
-    for (i=(anArrayToReverse.length-1); i>=0;) {
-        wordHolder=anArrayToReverse[i];
+    for (i = (anArrayToReverse.length - 1); i >= 0;) {
+        wordHolder = anArrayToReverse[i];
         reversedArray.push(wordHolder);
         i--;
     }
     console.log(reversedArray);
 }
 
-arrayReverse(arrayOfStrings);
+arrayReverse(myArray);
+
+/**
+ * Practice Problem 3
+ * @param futureMultidimensionalArray
+ */
+var multiTypeArray = ['hello', 34, true, false, 'goodbye', 56, 12, '25', true];
+
+function oneToMulti(futureMultidimensionalArray) {
+    var multiarray = [];
+    var arrayString = [];
+    var arrayNumber = [];
+    var arrayBoolean = [];
+    var checkTheType;
+    for(i=0; i<futureMultidimensionalArray.length; i++) {
+     checkTheType= futureMultidimensionalArray[i];
+        switch(typeof checkTheType) {
+            case 'string':
+                arrayString.push(checkTheType);
+                break;
+            case 'number':
+                arrayNumber.push(checkTheType);
+                break;
+            case 'boolean':
+                arrayBoolean.push(checkTheType);
+                break;
+        }
+
+    }
+    multiarray.push(arrayString);
+    multiarray.push(arrayNumber);
+    multiarray.push(arrayBoolean);
+    console.log(multiarray);
+}
+
+oneToMulti(multiTypeArray);
