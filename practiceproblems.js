eef5dbd5ff77408ac0b4c0b2c2c752e4b37dcd77
michpenn <michpenn@icloud.com>
2015-10-27 16:01:34 -0700

    Practice Problem 2

--- practiceproblems.js
+++ practiceproblems.js
@@ -11,9 +11,9 @@ var arrayOfStrings = ['bug', 'insect', 'mammal', 'reptile', 'mammoth', 'turtles'
 function biggerWords(theString, theArray) {
     var longWord = '';
     var longArray = [];
-    for(i=0; i<theArray.length; i++) {
-        if (theString.length <theArray[i].length) {
-           longWord = theArray[i];
+    for (i = 0; i < theArray.length; i++) {
+        if (theString.length < theArray[i].length) {
+            longWord = theArray[i];
             longArray.push(longWord);
         }
     }
@@ -21,3 +21,20 @@ function biggerWords(theString, theArray) {
 }
 
 biggerWords('hello', arrayOfStrings);
+
+/**
+ * Practice Problem 2
+ * @param anArrayToReverse
+ */
+function arrayReverse(anArrayToReverse) {
+    var reversedArray=[];
+    var wordHolder;
+    for (i=(anArrayToReverse.length-1); i>=0;) {
+        wordHolder=anArrayToReverse[i];
+        reversedArray.push(wordHolder);
+        i--;
+    }
+    console.log(reversedArray);
+}
+
+arrayReverse(arrayOfStrings);
